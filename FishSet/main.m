b0dd76382dd707e0fac2ced71f27b58f804e696f
John Saba <john.thomas.saba@gmail.com>
2013-01-19 09:21:55 -0600

    first commit

---
+++ FishSet/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FishSet
+//
+//  Created by John Saba on 1/18/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+        return retVal;
+    }
+}
