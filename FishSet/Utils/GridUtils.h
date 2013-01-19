599b5351ce489fa40e414af9b8512ca6425a5cab
John Saba <john.thomas.saba@gmail.com>
2013-01-19 16:28:01 -0600

    Add GridUtils cont...
    ""

--- FishSet/Utils/GridUtils.h
+++ FishSet/Utils/GridUtils.h
@@ -8,6 +8,28 @@
 
 #import <Foundation/Foundation.h>
 
+typedef struct
+{
+    int x;
+    int y;
+    
+} GridCoord;
+
+// helper function to create a GridCoord
+static inline GridCoord
+GridCoordMake(const NSInteger x, const NSInteger y)
+{
+	GridCoord gridCoord = {x, y};
+	return gridCoord;
+}
+
+
 @interface GridUtils : NSObject
+
+// absolute center position on a grid for grid coordinate
++ (CGPoint) absolutePositionForGridCoord:(GridCoord)coord unitSize:(CGFloat)unitSize origin:(CGPoint)origin;
+
+// grid coordinate for absolute position on a grid (any position inside a grid unit)
++ (GridCoord) gridCoordForAbsolutePosition:(CGPoint)position unitSize:(CGFloat)unitSize origin:(CGPoint)origin;
 
 @end
--- FishSet/Utils/GridUtils.m
+++ FishSet/Utils/GridUtils.m
@@ -10,4 +10,20 @@
 
 @implementation GridUtils
 
+// absolute position on a grid for grid coordinate, bottom left
++ (CGPoint) absolutePositionForGridCoord:(GridCoord)coord unitSize:(CGFloat)unitSize origin:(CGPoint)origin
+{
+    CGFloat x = (coord.x * unitSize) + origin.x;
+    CGFloat y = (coord.y * unitSize) + origin.y;
+    return CGPointMake(x, y);
+}
+
+// grid coordinate for absolute position on a grid
++ (GridCoord) gridCoordForAbsolutePosition:(CGPoint)position unitSize:(CGFloat)unitSize origin:(CGPoint)origin;
+{
+    NSInteger x = floorf((position.x - origin.x) / unitSize);
+    NSInteger y = floorf((position.y - origin.y) / unitSize);
+    return GridCoordMake(x, y);
+}
+
 @end
